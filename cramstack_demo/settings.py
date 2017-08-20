120db8a016865db6b248746249d9b685746fc73f
--replace <mazg1493@gmail.com>
2017-08-20 12:29:21 +0600

    Modifying JWT token time to seven days instead of default 30 seconds.

--- cramstack_demo/settings.py
+++ cramstack_demo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import datetime
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -178,3 +179,8 @@ MEDIA_URL = '/media/'
 
 
 REST_USE_JWT = True
+
+JWT_AUTH = {
+    'JWT_ALLOW_REFRESH': True,
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=7)
+}
