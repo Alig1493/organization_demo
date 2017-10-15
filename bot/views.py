8d704741e76a9f3464681cdf59065c14c7f33ff8
ali_cramstack <mazg1493@gmail.com>
2017-10-15 19:54:32 +0600

    Some progress

--- bot/views.py
+++ bot/views.py
@@ -24,6 +24,7 @@ class Message(generics.ListCreateAPIView):
             super().create(request, *args, **kwargs)
         except Exception as e:
             print(e)
+            return HttpResponse()
 
 
     # def create(self, request, *args, **kwargs):
